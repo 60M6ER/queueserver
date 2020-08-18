4605746c7f3467b716c52670def48a116e812477
Boris <79999987388@ya.ru>
2020-08-18 10:24:54 +0300

    18.08.2020

--- src/main/java/com/baikalsr/queueserver/Application.java
+++ src/main/java/com/baikalsr/queueserver/Application.java
@@ -2,10 +2,12 @@ package com.baikalsr.queueserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan({"com.baikalsr.queueserver.*"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
